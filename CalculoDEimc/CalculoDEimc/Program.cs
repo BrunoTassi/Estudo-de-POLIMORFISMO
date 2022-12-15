3ccba473fd96dc7c9eec0ba4e55f55a61f3c9f7d
Bruno Tassi <brunoxtassi923@gmail.com>
2022-12-14 21:12:53 -0300

    commit do conversor

---
+++ CalculoDEimc/CalculoDEimc/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoDEimc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //calculo de imc 
+            //dividir peso pela altura ao quadrado em metros 
+          
+
+            Console.WriteLine("CALCULADORA DE IMC");
+            Console.WriteLine("");
+
+            Console.WriteLine("Digite quantos Kg voce possui atualmente : ");
+            double peso = double.Parse(Console.ReadLine());
+           
+            Console.WriteLine("-----------------------------------");
+           
+            Console.WriteLine("Digite sua altura em metros : ");
+            double altura = double.Parse(Console.ReadLine());
+
+            // calculo do imc 
+            double resultado = peso / (altura * altura);
+            
+            if (resultado < 18.5)
+            {
+                Console.WriteLine("\n IMC = Magreza" + " o seu imc é " +resultado );
+            }
+
+            else if (resultado >= 18.6 && resultado <= 24.9)
+            {
+                Console.WriteLine("\nIMC = Normal " + " o seu imc é " + resultado);
+            }
+            else if (resultado >25 && resultado <= 29.9) 
+            {
+                Console.WriteLine("\nIMC = Sobrepeso " + "o seu imc é " + resultado);
+            }
+            else if (resultado >=30 && resultado <= 39.9) 
+            {
+                Console.WriteLine("\nIMC = Obesidade " + " o seu imc é " + resultado);
+            }
+            else
+            {
+                Console.WriteLine("\n IMC = OBESIDADE GRAVE " + " o seu imc é " + resultado);   
+            }
+            // imc = menor que 18.5 - magreza , imc entre 18.5 e 24.9 normal 
+            // imc = entre 25 a 29.9 sobrepeso , entre 30 e 39.9 obesidade
+            // imc = maior que 40 obesidade grave 
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConversorDeTemperatura/ConversorDeTemperatura/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorDeTemperatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region captura de informação
+            // captura de informação do usuario
+
+            double c,f,k;
+            Console.WriteLine("###CONVERSOR DE TEMPERATURAS###");
+            Console.Write("insira a temperatura em CELSIUS : ");
+            c = double.Parse ( Console.ReadLine());
+            Console.WriteLine("--------------------------------");
+            #endregion
+
+            #region METODO DE CALCULOS PARA CONVERTER
+
+            //calculo para conversão de celsius para Farenheit
+            f = ((c * 9) + 160) / 5;
+            //para Kelvin
+            k = c + 273.15;
+
+
+            #endregion
+            
+            
+            Console.WriteLine(c + " de temperatura SELCIUS convertida em escala KELVIN = " + k +" graus Kelvin");
+        
+            Console.WriteLine(c + " de temperatura SELCIUS convertira em escala FARENHEIT = " + f + " graus Farenheit");
+
+            Console.WriteLine("*********************************");
+            Console.ReadKey();
+        }
+    }
+}
